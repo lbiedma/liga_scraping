a1118ed5f4fb9b61328903055abb17e6c4722ee0
Luis Biedma <lbiedma@unc.edu.ar>
2025-07-17 18:23:52 -0300

    agregado defensivo

--- create_four_factors_analysis.py
+++ create_four_factors_analysis.py
@@ -11,22 +11,24 @@ def create_fgm_fga_columns(df: pd.DataFrame) -> pd.DataFrame:
     df["FTA"] = df["ft_A/I_FGA"]
     return df
 
-def create_opp_rebs(df: pd.DataFrame, opp_df: pd.DataFrame) -> pd.DataFrame:
-    df["opp_ORB"] = opp_df["Ofe."]
-    df["opp_DRB"] = opp_df["Def."]
+def create_opp_rebs(final_stats: pd.DataFrame, final_stats_opp: pd.DataFrame) -> pd.DataFrame:
+    final_stats["opp_ORB"] = final_stats_opp["Ofe."]
+    final_stats["opp_DRB"] = final_stats_opp["Def."]
 
-    return df
+    return final_stats
 
 def create_four_factors_analysis(df: pd.DataFrame, opp_df: pd.DataFrame) -> pd.DataFrame:
     """
     Create a four factors analysis for a given dataframe.
     """
     df = create_fgm_fga_columns(df)
-    df = create_opp_rebs(df, opp_df)
+    final_stats_opp = opp_df.sum(numeric_only=True)
     final_stats = df.sum(numeric_only=True)
+    final_stats = create_opp_rebs(final_stats, final_stats_opp)
     final_stats["effective_fg_pct"] = (final_stats["2p_A/I_FGM"] + 1.5 * final_stats["3p_A/I_FGM"]) / (final_stats["2p_A/I_FGA"] + final_stats["3p_A/I_FGA"])
     final_stats["tov_factor"] = final_stats["PÉR"] / (final_stats["FGA"] + 0.44 * final_stats["FTA"] + final_stats["PÉR"])
     final_stats["off_reb_factor"] = final_stats["Ofe."] / (final_stats["Ofe."] + final_stats["opp_DRB"])
+    final_stats["def_reb_factor"] = final_stats["Def."] / (final_stats["Def."] + final_stats["opp_ORB"])
     final_stats["ft_factor"] = final_stats["ft_A/I_FGA"] / final_stats["FGA"]
     final_stats["four_off_factors_score"] = 0.4 * final_stats["effective_fg_pct"] + 0.25 * final_stats["tov_factor"] + 0.2 * final_stats["off_reb_factor"] + 0.15 * final_stats["ft_factor"]
 
